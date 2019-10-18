88ca6010ab2a84efbd6b3dfd35661766e044a5cd
Miguel Gimenez <miguel.gimenez@primaverasound.com>
2019-10-18 09:52:21 +0200

    fix

--- config/dev.js
+++ config/dev.js
@@ -1,12 +1,11 @@
 // Don't be like me and commit this file!
 // These keys have been disabled, but remain here for reference purposes.
 module.exports = {
-  googleClientID:
-    '26972040642-261296sbtvtq1738h8sqh9ca7qqodme2.apps.googleusercontent.com',
-  googleClientSecret: 'RFh5qveO7HZMPL9D3tQ76gQC',
-  mongoURI: 'mongodb://stephen:password@ds239128.mlab.com:39128/blog_dev',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379',
-  accessKeyId: 'AKIAIWSXFHRM7F6Z3NWQ',
-  secretAccessKey: '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'
+	googleClientID: '26972040642-261296sbtvtq1738h8sqh9ca7qqodme2.apps.googleusercontent.com',
+	googleClientSecret: 'RFh5qveO7HZMPL9D3tQ76gQC',
+	mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+	cookieKey: '123123123',
+	redisUrl: 'redis://127.0.0.1:6379',
+	accessKeyId: 'AKIAIWSXFHRM7F6Z3NWQ',
+	secretAccessKey: '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'
 };
