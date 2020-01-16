1b2a30819cec1206d849a4744eb7dbaffe7cee8a
Miguel Gimenez <miguel.gimenez@primaverasound.com>
2020-01-16 10:31:46 +0100

    uncomment

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./models/Blog');
 require('./services/passport');
-// require('./services/cache');
+require('./services/cache');
 // NEW COMMIT
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
